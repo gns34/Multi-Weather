864dc0166d218e0293a1506a55e7639580a8e74b
Gopi Nath <gnsgns199@gmail.com>
2024-12-19 23:27:24 +0530

    Images added and configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import img from './assets/weather.png'
+import img2 from './assets/heavy-rain.png'
+import img3 from './assets/rainy-day.png'
 function App() {
 
   return (
@@ -7,9 +9,17 @@ function App() {
     <div class="font-bold text-3xl text-center mt-10">
     <h1>Modern Multi-Weather App</h1>
     </div>
-    <img src={img} alt="weather logo" />
+    <img src={img2} alt="weather logo2" />
+    <img src={img} alt="weather logo1" />
+    <img src={img3} alt="weather logo3" />
+  
+    
+    <br></br><br></br>
 
-      
+    {/* For getting weather location i.e.hometown */}
+    <label for="fname">Location: </label>
+    <input type="text" id="loc" name="locname"></input>
+
     </>
   )
 }
