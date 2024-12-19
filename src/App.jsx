d6af6bf0f8ac75ee946e48c779c09fbff1e270ff
Gopi Nath <gnsgns199@gmail.com>
2024-12-19 23:13:51 +0530

    Refactor file paths and remove unused CSS file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import img from './assets/weather.png'
 function App() {
 
   return (
     <>
     <div class="font-bold text-3xl text-center mt-10">
     <h1>Modern Multi-Weather App</h1>
-      <img src="" alt="" />
     </div>
+    <img src={img} alt="weather logo" />
+
       
     </>
   )
