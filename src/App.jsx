cb30b33b13743dc5724590c5108828711c2e67d3
Gopi Nath <gnsgns199@gmail.com>
2024-12-19 22:25:03 +0530

    Tailwind CSS Configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-
 import './App.css'
-
 function App() {
 
   return (
     <>
     <div class="font-bold text-3xl text-center mt-10">
     <h1>Modern Multi-Weather App</h1>
-    
+      <img src="" alt="" />
     </div>
       
     </>
