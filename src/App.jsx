4553de7065df4eb7e3337762e02734c9d45bbfa1
Gopi Nath <gnsgns199@gmail.com>
2024-12-30 00:14:25 +0530

    Add weather API integration and environment configuration

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import './App.css'
 import img from './assets/weather.png'
 import img2 from './assets/heavy-rain.png'
 import img3 from './assets/rainy-day.png'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 function App() {
 
   const [location, setLocation] = useState('')
+  const search= async(cityname)=>{
+    try {
+      const url=`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${import.meta.env.VITE_API_KEY}`;
+      const response=await fetch(url);
+      const data=await response.json();
+      console.log(data);
+    } catch (error) {
+      //later to defined
+    }
+  }
   
   return (
     <>
@@ -22,6 +32,7 @@ function App() {
     <label for="fname">Location: </label>
     <input type='text' onChange={e=>setLocation(e.target.value)}></input>
     <h1>You are in: {location}</h1>
+    <h2>Temperature : </h2>
     
     </>
   )
